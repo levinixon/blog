3f6c73f0d4bf81bc64cfcd70412732482f8fa024
levinixon <108950890+levinixon@users.noreply.github.com>
2023-01-22 15:57:51 +0000

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between px-10 py-5 mt-10'>
         <div>
-            <h1 className='text-7xl font-bold'>Levi Nixon's Blog</h1>
+            <h1 className='text-7xl font-bold'>The Blog</h1>
             <h2 className='mt-5 md:mt-2'>Welcome to the weekly blog of UK based full-stack software engineer.</h2>
 
         </div>
